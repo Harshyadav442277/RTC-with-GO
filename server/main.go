c4e2ba18fd3b706e8d465a963e743edcf8ec4fb9
Harshyadav442277 <hyadav42774@gmail.com>
2026-06-24 10:23:44 +0530

    handling multiple clients persistently

--- server/main.go
+++ server/main.go
@@ -1,40 +1,39 @@
 package main
-
-import (
+import(
 	"fmt"
 	"net"
+	"bufio"
 )
-
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn){
 	defer conn.Close()
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading bytes")
-	}
-	fmt.Printf("Received: %s  from Client Address %s\n\n", buf[:n], conn.RemoteAddr())
-
-	//Write back to go server
-	conn.Write([]byte("Hello from GO server"))
-
-}
-func main() {
-
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Println("Error listening port:", err)
-	}
-	fmt.Println("Server listening on port 8080")
-	defer listener.Close()
-
-	for i := 0; i < 5; i++ {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting client:", err)
+	addr := conn.RemoteAddr().String()
+	fmt.Println("Client Connected: ", addr)
+	reader := bufio.NewReader(conn)
+	for{
+		message, err := reader.ReadString('\n')
+		if err != nil{
+			fmt.Println("Client disconnected: ", addr)
+			return
 		}
-		fmt.Println("Client connected:", conn.RemoteAddr())
+		fmt.Printf("%s: %s\n", addr, message)
 
+		conn.Write([]byte("Echo: " + message))
+	}
+}
+func main(){
+	listener, err := net.Listen("tcp", ":8080")
+	if err!= nil{
+		fmt.Println("Error starting the server:", err)
+		return
+	}
+	fmt.Println("Server running on port 8080")
+	defer listener.Close()
+	for{
+		conn, err := listener.Accept()
+		if err!= nil{
+			fmt.Println("Error Accepting: ", err)
+			continue
+		}
 		go handleClient(conn)
-
 	}
 }
