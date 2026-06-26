436831d91c58dd8d222eb296eca96b3a9562dd31
Harshyadav442277 <hyadav42774@gmail.com>
2026-06-26 12:38:52 +0530

    Client side added

---
+++ client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Could not connect: ", err)
+		return
+	}
+	fmt.Println("Connected! Type message")
+	defer conn.Close()
+	go func() {
+		ServerReader := bufio.NewReader(conn)
+		for {
+			message, err := ServerReader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Connection refused: ", err)
+				os.Exit(0)
+			}
+			fmt.Println("Server ---> ", message)
+		}
+	}()
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		conn.Write([]byte(text + "\n"))
+	}
+}
